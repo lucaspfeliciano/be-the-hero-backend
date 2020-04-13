9c5e3ed88a9a0d0f7bec44faf18b3659444e0b5e
lucaspfeliciano <lucaspfeliciano@hotmail.com>
2020-04-13 14:29:31 -0300

    Cria estrutura do projeto, configura db e cria rotas de ongs

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const routes = express.Router();
+
+const OngController = require('./controllers/OngController');
+
+routes.get('/ongs', OngController.list)
+routes.post('/ongs', OngController.create)
+
+module.exports = routes;
